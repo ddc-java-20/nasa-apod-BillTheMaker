eeec6c702e4cd7983fcd43f36738e92098750682
Bill <wholcombe@cnm.edu>
2025-02-25 10:41:41 -0700

    completed Apod entity class.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/model/entity/Apod.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/entity/Apod.java
@@ -3,13 +3,20 @@ package edu.cnm.deepdive.nasaapod.model.entity;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import java.net.URL;
 import java.time.LocalDate;
 
-@Entity(tableName = "apod")
+@Entity(
+    tableName = "apod",
+    indices = {
+        @Index(value = {"date"}, unique = true)
+    }
+)
+
 public class Apod {
 
   @PrimaryKey(autoGenerate = true)
