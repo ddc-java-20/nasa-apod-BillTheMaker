7cae6b770dc18803dd1418d5e2fa5c68e9b1f73d
Bill <wholcombe@cnm.edu>
2025-02-25 09:29:08 -0700

    Starting service proxy and model classes

---
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/service/ApodService.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.nasaapod.service;
+
+import edu.cnm.deepdive.nasaapod.model.entity.Apod;
+import io.reactivex.rxjava3.core.Single;
+import java.time.LocalDate;
+import java.util.List;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApodService {
+
+  @GET("apod")
+  Single<List<Apod>> getDateRange(
+      @Query("start_Date") LocalDate startDate,
+      @Query("end_Date") LocalDate endDate,
+      @Query("api_key") String apiKey);
+}
