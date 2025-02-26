f9ca53a14f83276fdce1799e046106c137a1bfed
Bill <wholcombe@cnm.edu>
2025-02-26 14:17:43 -0700

    current apod displayed in activity

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -10,11 +10,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import com.squareup.picasso.Picasso;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.databinding.ActivityMainBinding;
 import edu.cnm.deepdive.nasaapod.model.entity.Apod;
 import edu.cnm.deepdive.nasaapod.viewmodel.ApodViewModel;
+import javax.inject.Inject;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
@@ -24,18 +26,36 @@ public class MainActivity extends AppCompatActivity {
   private ActivityMainBinding binding;
   private ApodViewModel viewModel;
 
+  @Inject
+  Picasso picasso;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setupUI();
+    setupViewModel();
+  }
+
+  private void setupUI() {
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     // TODO: 2/26/2025 attach listeners to view widgets (fields in binding) todo
     // TODO: 2/26/2025 update state of view widgets
     setContentView(binding.getRoot());
+  }
+
+  private void setupViewModel() {
     viewModel = new ViewModelProvider(this).get(ApodViewModel.class);
     viewModel
         .getApod()
-        .observe(this, (apod) ->
-            Log.d(TAG, "onChanged: " + apod));
+        .observe(this, (apod) -> {
+          if (apod != null) {
+            // TODO: 2/26/2025 Grab image HOW? and put in ImageView widget.
+            picasso
+                .load(apod.getLowDefUrl().toString())
+                .into(binding.image);
+              }
+            });
+
     viewModel.setToday();
   }
 
